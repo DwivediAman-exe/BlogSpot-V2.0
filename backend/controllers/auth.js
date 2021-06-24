6b555be35c6d64b2556d735ee4545f5205939b57
Aman Dwivedi <19dwivediaman@gmail.com>
2021-06-25 02:11:37 +0530

    user sign up

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,50 @@
-exports.signup = (req, res) => {
+const User = require('../models/user');
+const shortId = require('shortid');
+
+// exports.signup = (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   User.findOne({ email }).exec((err, user) => {
+//     if (user) {
+//       return res.status(400).json({ error: 'Email is already in use' });
+//     }
+
+//     let username = shortId.generate();
+//     let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+//     let newUser = new User({ name, email, password, profile, username });
+
+//     newUser.save((err, success) => {
+//       if (err) return res.status(400).json({ error: err });
+//       res.json({
+//         user: newUser,
+//       });
+//       // res.json({ message: 'Signup Successful ! Please LogIn' });
+//     });
+//   });
+// };
+
+exports.signup = async (req, res) => {
   const { name, email, password } = req.body;
-  res.json({
-    user: { name, email, password },
-  });
+
+  try {
+    // checking if the user is unique
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'Email is already in use' });
+    }
+
+    // generating username & profile then saving newUser
+    let username = shortId.generate();
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+
+    let newUser = new User({ name, email, password, profile, username });
+
+    await newUser.save().then(() => {
+      res.json({ message: 'Signup Successful ! Please LogIn to continue' });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('SignUp Server error');
+  }
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -53,4 +53,38 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// virtual field since direct passwords cannot be saved in database
+userSchema
+  .virtual('password')
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + '';
+  },
+};
+
 module.exports = mongoose.model('User', userSchema);
